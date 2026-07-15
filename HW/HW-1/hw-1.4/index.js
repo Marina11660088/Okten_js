74c50e3f11878bcd9a3542d008bdda0309250667
Marina11660088 <marinka.um@gmail.com>
2026-07-15 14:52:09 +0200

    hw-2

---
+++ HW/HW-1/hw-1.4/index.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Введіть ваше ім'я");
+console.log(firstName);
+let middleName = prompt("Введіть ваше по-батькові");
+console.log(middleName);
+let lastName = prompt("Введіть ваше призвище");
+console.log(lastName);
